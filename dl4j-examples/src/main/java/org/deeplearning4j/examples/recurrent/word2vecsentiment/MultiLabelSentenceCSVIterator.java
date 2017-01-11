870e7cd2f9c4fb34292c4ba9c36e23c3c2184212
Ryan <ryan@zymewire.com>
2017-01-10 21:37:36 -0500

    Multi label categorization and loading from csv

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/MultiLabelSentenceCSVIterator.java
@@ -0,0 +1,255 @@
+package org.deeplearning4j.examples.recurrent.word2vecsentiment;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.LineIterator;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+/**
+ * Created by ryan on 10/01/17.
+ */
+public class MultiLabelSentenceCSVIterator implements DataSetIterator {
+
+    private final int vectorSize;
+    private final WordVectors wordVectors;
+    private final int number_of_labels;
+    private final int truncateLength;
+    private LineIterator iterator;
+    private final int batch_size;
+    private int cursor = 0;
+    private final TokenizerFactory tokenizerFactory;
+
+
+
+    public MultiLabelSentenceCSVIterator(String file, WordVectors wordVectors, int number_of_labels, int batchSize, int truncateLength) throws Exception{
+        this.vectorSize = wordVectors.getWordVector(wordVectors.vocab().wordAtIndex(0)).length;
+        this.wordVectors = wordVectors;
+        this.number_of_labels = number_of_labels;
+        this.truncateLength = truncateLength;
+
+
+        //First: get the dataset using the record reader. CSVRecordReader handles loading/parsing
+        int numLinesToSkip = 0;
+        String delimiter = ",";
+        this.iterator = FileUtils.lineIterator(new File(file), "UTF-8");
+        this.batch_size = batchSize;
+        //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
+        int labelIndex = 1;     //First cell is the text string, second is the label
+
+        tokenizerFactory = new DefaultTokenizerFactory();
+        tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
+
+    }
+
+
+    @Override
+    public DataSet next(int num) {
+        if (!this.iterator.hasNext()) throw new NoSuchElementException();
+        try{
+            return nextDataSet(num);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private DataSet nextDataSet(int num) throws IOException {
+        //First: load reviews to String. Alternate positive and negative reviews
+        List<String> reviews = new ArrayList<>(num);
+        int[] label = new int[num];
+        for( int i=0; i<num && iterator.hasNext(); i++ ){
+            String[] line = iterator.nextLine().split(":=:");
+            if(line.length > 1){
+                label[i] = Integer.parseInt(line[1].trim());
+                reviews.add(line[0].trim());
+                cursor++;
+            }
+//
+//
+//
+//            if(cursor % pos_neg_splitter == 0){
+//                //Load positive review
+//                int posReviewNumber = posReviewNumberFor(cursor);
+//                if(positiveFiles.length > posReviewNumber) {
+//                    String review = FileUtils.readFileToString(positiveFiles[posReviewNumber]);
+//                    reviews.add(review);
+//                    positive[i] = true;
+//                }
+//            } else {
+//                //Load negative review
+//                int negReviewNumber = negReviewNumberFor(cursor);
+//                if(negativeFiles.length > negReviewNumber) {
+//                    String review = FileUtils.readFileToString(negativeFiles[negReviewNumber]);
+//                    reviews.add(review);
+//                    positive[i] = false;
+//                }
+//            }
+//            cursor++;
+        }
+
+        //Second: tokenize reviews and filter out unknown words
+        List<List<String>> allTokens = new ArrayList<>(reviews.size());
+        int maxLength = 0;
+        for(String s : reviews){
+            List<String> tokens = tokenizerFactory.create(s).getTokens();
+            List<String> tokensFiltered = new ArrayList<>();
+            for(String t : tokens ){
+                if(wordVectors.hasWord(t)) tokensFiltered.add(t);
+            }
+            allTokens.add(tokensFiltered);
+            maxLength = Math.max(maxLength,tokensFiltered.size());
+        }
+
+        //If longest review exceeds 'truncateLength': only take the first 'truncateLength' words
+        if(maxLength > truncateLength) maxLength = truncateLength;
+
+        //Create data for training
+        //Here: we have reviews.size() examples of varying lengths
+        INDArray features = Nd4j.create(reviews.size(), vectorSize, maxLength);
+        INDArray labels = Nd4j.create(reviews.size(), this.number_of_labels, maxLength);    //Two labels: positive or negative
+        //Because we are dealing with reviews of different lengths and only one output at the final time step: use padding arrays
+        //Mask arrays contain 1 if data is present at that time step for that example, or 0 if data is just padding
+        INDArray featuresMask = Nd4j.zeros(reviews.size(), maxLength);
+        INDArray labelsMask = Nd4j.zeros(reviews.size(), maxLength);
+
+        int[] temp = new int[2];
+        for( int i=0; i<reviews.size(); i++ ){
+            List<String> tokens = allTokens.get(i);
+            temp[0] = i;
+            //Get word vectors for each word in review, and put them in the training data
+            for( int j=0; j<tokens.size() && j<maxLength; j++ ){
+                String token = tokens.get(j);
+                INDArray vector = wordVectors.getWordVectorMatrix(token);
+                features.put(new INDArrayIndex[]{NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.point(j)}, vector);
+
+                temp[1] = j;
+                featuresMask.putScalar(temp, 1.0);  //Word is present (not padding) for this example + time step -> 1.0 in features mask
+            }
+
+            //int idx = (positive[i] ? 0 : 1);
+            int lastIdx = Math.min(tokens.size(),maxLength);
+            labels.putScalar(new int[]{i,label[i],lastIdx-1},1.0);   //Set label: [0,1] for negative, [1,0] for positive
+            labelsMask.putScalar(new int[]{i,lastIdx-1},1.0);   //Specify that an output exists at the final time step for this example
+        }
+
+        return new DataSet(features,labels,featuresMask,labelsMask);
+    }
+
+    /**
+     * Used post training to convert a String to a features INDArray that can be passed to the network output method
+     *
+     * @param reviewContents Contents of the review to vectorize
+     * @param maxLength Maximum length (if review is longer than this: truncate to maxLength). Use Integer.MAX_VALUE to not nruncate
+     * @return Features array for the given input String
+     */
+    public INDArray loadFeaturesFromString(String reviewContents, int maxLength){
+        List<String> tokens = tokenizerFactory.create(reviewContents).getTokens();
+        List<String> tokensFiltered = new ArrayList<>();
+        for(String t : tokens ){
+            if(wordVectors.hasWord(t)) tokensFiltered.add(t);
+        }
+        int outputLength = Math.max(maxLength,tokensFiltered.size());
+
+        INDArray features = Nd4j.create(1, vectorSize, outputLength);
+
+        for( int j=0; j<tokens.size() && j<maxLength; j++ ){
+            String token = tokens.get(j);
+            INDArray vector = wordVectors.getWordVectorMatrix(token);
+            features.put(new INDArrayIndex[]{NDArrayIndex.point(0), NDArrayIndex.all(), NDArrayIndex.point(j)}, vector);
+        }
+
+        return features;
+    }
+
+    @Override
+    public int totalExamples() {
+        return 0;
+    }
+
+    @Override
+    public int inputColumns() {
+        return 0;
+    }
+
+    @Override
+    public int totalOutcomes() {
+        return 0;
+    }
+
+    @Override
+    public boolean resetSupported() {
+        return false;
+    }
+
+    @Override
+    public boolean asyncSupported() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public int batch() {
+        return batch_size;
+    }
+
+    @Override
+    public int cursor() {
+        return cursor;
+    }
+
+    @Override
+    public int numExamples() {
+        return 0;
+    }
+
+    @Override
+    public void setPreProcessor(DataSetPreProcessor dataSetPreProcessor) {
+
+    }
+
+    @Override
+    public DataSetPreProcessor getPreProcessor() {
+        return null;
+    }
+
+    @Override
+    public List<String> getLabels() {
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public DataSet next() {
+        return next(batch_size);
+    }
+}
