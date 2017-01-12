8693ddc7675a05c1f34c6c020000e5ab70345cc0
ryan <ryan@zymewire.com>
2017-01-11 20:49:05 -0500

    Added learning rate and regularization addition by ars to main class

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/MultiLabelClassification.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/MultiLabelClassification.java
@@ -7,11 +7,17 @@ public class MultiLabelClassification {
 
     public static void main(String[] args) throws Exception{
         String train_file = "/home/ryan/projects/dl_service/test_multi_lable.txt";
-        String test_file = train_file;
+        String test_file = "/home/ryan/projects/dl_service/test_multi_lable.txt";
         String word2vec_file = "/home/ryan/projects/dl_service/deps.words";
         int number_of_labels = 3;
         double learning_rate = 0.25;
+        if(args.length > 0){
+            learning_rate = Double.parseDouble(args[0]);
+        }
         double regularization = 0.001;
+        if(args.length > 1){
+            regularization = Double.parseDouble(args[1]);
+        }
         int epochs = 3;
         Word2VecMultiLabelCategorisationRNN classifier = new Word2VecMultiLabelCategorisationRNN(train_file, test_file, word2vec_file, number_of_labels, learning_rate, regularization, epochs);
         classifier.trainAndTest();
