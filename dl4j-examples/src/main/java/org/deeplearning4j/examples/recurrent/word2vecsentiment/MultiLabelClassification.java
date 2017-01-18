069bd37fa1f50e4387960bb9e50dc5130a923bf4
Ryan <ryan@zymewire.com>
2017-01-18 11:40:55 -0500

    Added ability to point at specific collection of positive and negative data with differing ratios pos vs neg.

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/MultiLabelClassification.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/word2vecsentiment/MultiLabelClassification.java
@@ -1,8 +1,6 @@
 package org.deeplearning4j.examples.recurrent.word2vecsentiment;
 
-import org.nd4j.jita.conf.CudaEnvironment;
-import org.nd4j.linalg.api.buffer.DataBuffer;
-import org.nd4j.linalg.api.buffer.util.DataTypeUtil;
+
 
 /**
  * Created by ryan on 11/01/17.
@@ -15,12 +13,6 @@ public class MultiLabelClassification {
         String word2vec_file = "/home/ryan/projects/dl_service/deps.words";
         int number_of_labels = 3;
 
-//        DataTypeUtil.setDTypeForContext(DataBuffer.Type.HALF);
-//        CudaEnvironment.getInstance().getConfiguration()
-//            .setMaximumDeviceCacheableLength(1024 * 1024 * 1024L)
-//            .setMaximumDeviceCache(12L * 1024 * 1024 * 1024L)
-//            .setMaximumHostCacheableLength(1024 * 1024 * 1024L)
-//            .setMaximumHostCache(12L * 1024 * 1024 * 1024L);
 
         Word2VecMultiLabelCategorisationRNN classifier = new Word2VecMultiLabelCategorisationRNN(train_file, test_file, word2vec_file, number_of_labels);
 
